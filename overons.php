4afeecea9327ea9ce08493e9cdd2d8d1e7708625
loekvdloo <1206994@student.roc-nijmegen.nl>
2024-03-07 16:15:59 +0100

    header en footer gemaakt. ik heb ook de pagina's van het menu, bestellen en over ons aangemaakt en daar de header en footer ook toegevoegd

--- overons.php
+++ overons.php
@@ -8,18 +8,18 @@
 <body>
 <header>
     <div class="logoplataantekst">
-   <a href="index.php"><img src="assets/img/coollogo_com-15727867.png" alt="logoplataan" id="logoplataan"></a>
+        <a href="index.php"><img src="assets/img/coollogo_com-15727867.png" alt="logoplataan" id="logoplataan"></a>
         <h1><a href="index.php">Plataan</a></h1>
     </div>
-<nav>
-    <a href="menu.php">menu</a>
-    <a href="bestellen.php">bestellen</a>
-    <a href="overons.php">over ons</a>
+    <nav>
+        <a href="menu.php">menu</a>
+        <a href="bestellen.php">bestellen</a>
+        <a href="overons.php">over ons</a>
 
-</nav>
+    </nav>
 </header>
 <main>
-<a>odashfasljdfsdlkjkl;fdofhndifshdfh</a>
+    <a>over ons</a>
 </main>
 <footer>
     <div class="mediafooter">
@@ -36,19 +36,16 @@
             <a>024 696 2097</a>
         </div>
     </div>
-        <div class="openingstijden">
-            <h3>ma:  gesloten</h3>
-            <h3>di: <span id="di"> gesloten</span></h3>
-            <h3>wo:  <span id="wo">12:00-21:00</span></h3>
-            <h3>do:  <span id="do">12:00-21:00</span></h3>
-            <h3>vr:  <span id="we">12:00-21:00</span></h3>
-            <h3>za:  <span id="we">12:00-21:00</span></h3>
-            <h3>zo:  <span id="we">13:00-21:00</span> </h3>
-        </div>
+    <div class="openingstijden">
+        <h3>ma:  gesloten</h3>
+        <h3>di: <span id="di"> gesloten</span></h3>
+        <h3>wo:  <span id="wo">12:00-21:00</span></h3>
+        <h3>do:  <span id="do">12:00-21:00</span></h3>
+        <h3>vr:  <span id="we">12:00-21:00</span></h3>
+        <h3>za:  <span id="we">12:00-21:00</span></h3>
+        <h3>zo:  <span id="we">13:00-21:00</span> </h3>
+    </div>
 
 </footer>
 </body>
 </html>
-
-
-
